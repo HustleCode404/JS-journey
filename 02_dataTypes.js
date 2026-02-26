af9d35925f9c55c269eafc009fa0185bcd936461
HustleCode404 <anchor006974@gmail.com>
2026-02-26 17:36:01 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(6+4) we are using NodeJS , not browser
+
+// console.log(6+4); console.log("Kanishko") code should be readable and clean.
+
+
+// console.log(6+4); 
+
+// console.log("Kanishko")
+
+let name = "kanishko"
+let age = 7
+let isPresent = true
+
+// number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone Value
+// undefined => not defined
+// Symbol => unique
+
+// Object
+
+console.log(typeof undefined ); // undifined
+console.log(typeof null); // Object
