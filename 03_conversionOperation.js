7b305008c61484601a8a71e2ff955d9edb04cd25
HustleCode404 <anchor006974@gmail.com>
2026-03-01 03:09:18 +0530

    conversion in datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,63 @@
+let score = "koni"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//Nan reffers to Not a Number
+
+// score = 33; output = Number
+// score = 33adc; output = NaN
+// score = "33"; output = String
+// score = null; output = 0
+// score = undefined; output = NaN
+// score = true/false; output = 1/0
+// score = "koni"; output = NaN
+
+// as per examinataion in number there are many cases with different result {(33) number,(33aed) NaN,("33") string,(null) 0,(undifined) NaN,(true/false) 1/0,("koni") NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// isLoggedIn = 1; output = true
+// isLoggedIn = "1"; output = true
+// isLoggedIn = -1; output = true
+// isLoggedIn = 0; output = false
+// isLoggedIn = undefined; output = false
+// isLoggedIn = null; output = false
+// isLoggedIn = "koni"; output = true
+// isLoggedIn = ""; output = false
+
+// as per examinataion in boolean there are some cases were result is false (0,undifined,null); true (1,-1,string,>1)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// someNumber = 33; output = 33 but converted into srting
+// isLoggedIn = -33; output = -33 but converted into srting
+// isLoggedIn = "33"; output = "33" but converted into srting
+// isLoggedIn = 0; output = 0 but converted into srting
+// isLoggedIn = undefined; output = undefined but converted into srting
+// isLoggedIn = null; output = null but converted into srting
+// isLoggedIn = "koni"; output = string
+// isLoggedIn = ""; output = BLANK but converted into srting
+
+// as per examinataion in string there are all converting into string but when "" is applied alone it is resulting BLANK
+
+
+
+//****************************************************************OPERATIONS**********************************************************************
+
